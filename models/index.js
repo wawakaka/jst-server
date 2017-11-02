d798e5b9db548c104d1eec5f1556f684b06592f0
mubarak <wawakaka72@gmail.com>
2017-11-02 18:09:55 +0700

    - add procfile - remove use_env_variable

--- models/index.js
+++ models/index.js
@@ -7,13 +7,13 @@ var basename = path.basename(module.filename);
 var env = process.env.NODE_ENV || 'development';
 var config = require(__dirname + '/../config/config.json')[env];
 var db = {};
-
-if (config.use_env_variable) {
-  var sequelize = new Sequelize(process.env[config.use_env_variable]);
-} else {
-  var sequelize = new Sequelize(config.database, config.username,
-      config.password, config);
-}
+//
+// if (config.use_env_variable) {
+//   var sequelize = new Sequelize(process.env[config.use_env_variable]);
+// } else {
+var sequelize = new Sequelize(config.database, config.username,
+    config.password, config);
+// }
 
 fs.readdirSync(__dirname).filter(function(file) {
   return (file.indexOf('.') !== 0) && (file !== basename) &&
