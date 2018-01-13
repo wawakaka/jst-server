e061223d419ec2a59ff46f8919cfd63f6bd1c956
mubarak <wawakaka72@gmail.com>
2018-01-13 12:35:58 +0700

    add new endpoint find user by kelas id

--- routes/jadwalkelas.js
+++ routes/jadwalkelas.js
@@ -23,6 +23,10 @@ router.delete('/:id/delete',
     passport.authenticate('bearer', {session: false}),
     deleteJadwal()
 );
+router.get('/user/:kelasid',
+    passport.authenticate('bearer', {session: false}),
+    getJadwalKelasUser()
+);
 
 function getAllJadwal() {
   return function(req, res) {
@@ -73,6 +77,7 @@ function getJadwal() {
         }
         else if (res.status(404)) {
           res.status(404).json({
+            status: 'failed',
             message: 'not found',
           });
         }
@@ -191,6 +196,55 @@ function deleteJadwal() {
           message: 'jadwal deleted',
           data: true,
         });
+      }).catch(function(err) {
+        res.json({
+          status: 'failed',
+          message: 'error' + err,
+        });
+        res.send(err);
+      });
+    } else if (req.user.length < 1) {
+      res.status(401).json({
+        status: 'failed',
+        message: 'authorization error',
+      });
+    } else {
+      res.json({
+        status: 'failed',
+        message: 'error',
+      });
+    }
+  };
+}
+
+function getJadwalKelasUser() {
+  return function(req, res) {
+    if (req.user.length >= 1) {
+      models.kelas.find({
+        where: {
+          id: req.params.kelasid,
+        },
+      }).then(function(kelas) {
+        console.log(kelas);
+        if (kelas) {
+          models.user.find({
+            where: {
+              email: kelas.user_email,
+            },
+          }).then(function(user) {
+            res.status(200).json({
+              status: 'success',
+              message: 'retrieve user',
+              data: user,
+            });
+          });
+        }
+        else if (res.status(404)) {
+          res.status(404).json({
+            status: 'failed',
+            message: 'not found',
+          });
+        }
       }).catch(function(err) {
         res.json({
           status: 'failed',
